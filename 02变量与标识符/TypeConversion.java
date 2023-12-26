4435eb9f9e703eaf89045aeed702f73d1b69e5f1
HankMiller <m18335784129@163.com>
2023-12-26 22:46:38 +0800

    2023.12.26 commit

--- 02变量与标识符/TypeConversion.java
+++ 02变量与标识符/TypeConversion.java
@@ -1,6 +1,19 @@
 class TypeConversion{
 	public static void main(String[] agrs){
+		String name = "马中豪";
+		int age = 27;
+		String gender = "男";
+		int weight = 70;
+		// marital status表示婚姻状况
+		boolean maritalStatus = false;
+		// contact information 表示联系方式
+		String contactInformation = "18335784129";
 		
-		System.out.println("");
+		System.out.println("name = " + name);
+		System.out.println("age = " + age);
+		System.out.println("gender = " + gender);
+		System.out.println("weight = " + weight);
+		System.out.println("maritalStatus = " + maritalStatus);
+		System.out.println("contactInformation = " + contactInformation);
 	}
 }
