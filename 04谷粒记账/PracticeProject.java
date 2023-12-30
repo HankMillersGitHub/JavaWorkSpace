6d02751253acb58506bb076d4b98d9fa6e9e7594
HankMiller <m18335784129@163.com>
2023-12-30 23:43:10 +0800

    2023.12.30 commit Module Package Project

--- 04谷粒记账/PracticeProject.java
+++ 04谷粒记账/PracticeProject.java
@@ -10,7 +10,12 @@ class PracticeProject{
 			查询收支明细时，显示所有的收入，支出明细列表
 			
 		*/ 
-		// while(int i = 1){
+		boolean falg = true;
+		// 初始金额
+		int balance = 10000;
+		// 记录收支信息
+		String info = "";
+		while(falg){
 			System.out.println("--------Hank记账软件--------\n");
 			System.out.println("        1. 收支明细        ");
 			System.out.println("        2. 登记收入        ");
@@ -18,9 +23,50 @@ class PracticeProject{
 			System.out.println("        4. 退    出        \n");
 			System.out.println("--------Hank记账软件--------");
 			System.out.print("        请选择1-4：");
+			// 用户输入 格式为char
 			char key = Utility.readMenuSelection();
-			System.out.println(key);
+			switch(key){
+				case '1':
+					System.out.println("--------当前收支明细记录--------");
+					System.out.println("收支\t账户金额\t收支金额\t说明");
+					System.out.println(info);
+					System.out.println("-------------------------------");
+					break;
+				case '2':
+					System.out.print("本次收入金额：");
+					// income 表示收入金额
+					int income = Utility.readNumber();
+					System.out.print("本次收入说明：");
+					// illustrate 表示支出的说明
+					String illustrate = Utility.readString();
+					balance += income;
+					info += "收入" + "\t" + balance + "\t\t" + income + "\t\t" + illustrate + "\n"; 
+					System.out.println("-------------登记完成-------------");
+					break;
+				case '3':
+					System.out.print("本次支出金额：");
+					// expenditure 表示支出金额
+					int expenditure = Utility.readNumber();
+					System.out.print("本次支出说明：");
+					// infomation 表示支出的说明
+					String infomation = Utility.readString();
+					balance -= expenditure;
+					info += "支出" + "\t" + balance + "\t\t" + expenditure + "\t\t" + infomation + "\n"; 
+					System.out.println("-------------登记完成-------------");
+					break;
+				// 退出的逻辑
+				case '4':
+					System.out.print("确认是否退出( Y / N ):");
+					char exit = Utility.readConfirmSelection();
+					if(exit == 'Y'){
+						falg = false;
+					}else{
+						break;
+					}
+					break;
+				
+			}
 
-		// }
+		}
 	}
 }
---
+++ JavaSECode/src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName : Hello
+ * Package : PACKAGE_NAME
+ * Description :
+ *
+ * @author : Hank
+ * @version : 1.0
+ * @Create 2023/12/30 16:38
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
