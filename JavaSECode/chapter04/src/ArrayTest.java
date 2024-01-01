70b79320114c1739021ed4265b9f70fd58f66748
HankMiller <m18335784129@163.com>
2024-01-01 21:02:46 +0800

    2024.1.1 new year

--- JavaSECode/chapter04/src/ArrayTest.java
+++ JavaSECode/chapter04/src/ArrayTest.java
@@ -1,14 +1,14 @@
 /**
- * ClassName : Hello
+ * ClassName : ArrayTest
  * Package : PACKAGE_NAME
  * Description :
  *
  * @author : Hank
  * @version : 1.0
- * @Create 2023/12/30 16:38
+ * @Create 2023/12/31 10:38
  */
-public class Hello {
+public class ArrayTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+
     }
 }
