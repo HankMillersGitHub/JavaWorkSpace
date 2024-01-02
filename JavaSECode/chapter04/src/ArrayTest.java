abc3d575a9f4d09aa87edb1b7f93d6e69028a6f8
HankMiller <m18335784129@163.com>
2024-01-02 23:31:59 +0800

    2024.1.2 commit Array

--- JavaSECode/chapter04/src/ArrayTest.java
+++ JavaSECode/chapter04/src/ArrayTest.java
@@ -1,14 +1,19 @@
 /**
- * ClassName : ArrayTest
- * Package : PACKAGE_NAME
- * Description :
+ * Classtame : ArrayTest
+ * Package : PACKAGE_tAME
+ * Descriptiot :
  *
- * @author : Hank
- * @version : 1.0
+ * @author : Hatk
+ * @versiot : 1.0
  * @Create 2023/12/31 10:38
  */
 public class ArrayTest {
     public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6,7};
 
+        for (int j: newArr) {
+            System.out.println(j);
+        }
     }
 }
+
