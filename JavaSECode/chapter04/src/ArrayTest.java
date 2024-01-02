4e7144c87a27544fe4adf8f74b7f453d81eefcbc
HankMiller <m18335784129@163.com>
2024-01-03 01:16:56 +0800

    2024.1.3 commit arrays complated

--- JavaSECode/chapter04/src/ArrayTest.java
+++ JavaSECode/chapter04/src/ArrayTest.java
@@ -9,11 +9,30 @@
  */
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5,6,7};
+// 1. boolean equals(int[] a,int[] b)比较两个数组的元素是否依次相等
+// int[] a = new int[]{1, 2, 3};
+// int[] b = new int[]{1, 2, 3};
+// System.out.println(Arrays.equals(a, b));
 
-        for (int j: newArr) {
-            System.out.println(j);
-        }
+// 2. String toString(int[] a); 输出数组元素信息
+// int[] a = new int[]{1,2,3};
+// System.out.println(Arrays.toString(a));
+
+// 3. void fill(int[] a,int val) 将指定值填充到数组之中
+// int[] a = new int[]{1,2,3};
+// Arrays.fill(a,10);
+// System.out.println(Arrays.toString(a));
+
+// 4. void sort(int[] a) 使用快速排序算法实现的排序
+// int[] a = new int[]{4,1,5,7,8,2};
+// Arrays.sort(a);
+// System.out.println(Arrays.toString(a));
+
+// 5. int binarySearch(int[] a,int key) 二分查找
+// int[] a = new int[]{1,2,3,4,5,6,7,8,9};
+// int index = Arrays.binarySearch(a,5);
+// System.out.println(index);
+
     }
 }
 
